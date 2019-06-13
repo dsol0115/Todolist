f6b8444aa38ced89fcefad010efb14b0a7103290
dsol0115 <dsol0115@naver.com>
2019-06-13 15:38:48 +0900

    include main.jsp

---
+++ Todo/src/main/java/kr/or/connect/todo/api/MainServlet.java
@@ -0,0 +1,59 @@
+package kr.or.connect.todo.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.or.connect.todo.dao.TodoDao;
+import kr.or.connect.todo.dto.TodoDto;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+			response.setCharacterEncoding("utf-8");
+			response.setContentType("application/json");
+
+			TodoDao dao = new TodoDao();
+
+			List<TodoDto> list = dao.getTodos();
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			String json = objectMapper.writeValueAsString(list);
+
+			/*
+			PrintWriter out = response.getWriter();
+			out.println(json);
+			out.close();
+			*/
+			
+			request.setAttribute("result", json);
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/main.jsp");
+	        requestDispatcher.forward(request, response);
+			
+	}
+
+}
